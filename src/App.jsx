2863d137ddcb390ec2d1ee47372c1bed83dbd0e1
Daniel Lourenço Affonso <86497631+Daniel-online@users.noreply.github.com>
2024-03-11 19:06:07 +0000

    actual app

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
+import staticImg from "./assets/react-core-concepts.png";
+
+const adjetivosReact = ["fundamentais", "cruciais", "importantes", "Must-have"];
+function numerosAleatorios(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 function Header() {
+  const randomAdjetivo = adjetivosReact[numerosAleatorios(0, 4)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={staticImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        Conceitos do React<div class="randomAdjetivo">{randomAdjetivo}</div>que
+        você precisará para qualquer aplicação JSX que estiver construindo.
       </p>
     </header>
   );
 }
-
+function CoreConcept() {
+  function cardConceitosReact() {}
+  return (
+    <li>
+      {cardConceitosReact[0].title}
+      <p>{cardConceitosReact[0].description}</p>
+    </li>
+  );
+}
 function App() {
   return (
     <div>
       <Header />;
       <main>
-        <h2>Time to get started!</h2>
+        <h2>Vamos começar!</h2>
       </main>
     </div>
   );
